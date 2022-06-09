264c4797e9f59e6b31ce812a559d5a48ade0a983
Goldeaur <34507362+Master-Goldeau@users.noreply.github.com>
2022-06-09 20:33:33 +0200

    refactoring moving compnents into component folder

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {PlayerDetailComponent} from "./player-detail/player-detail.component";
-import {PlayerListComponent} from "./player-list/player-list.component";
+import {PlayerDetailComponent} from "./components/player-detail/player-detail.component";
+import {PlayerListComponent} from "./components/player-list/player-list.component";
 
 const routes: Routes = [
   {path: '', component:PlayerListComponent},
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,22 +3,24 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {PlayerComponent} from "./player/player.component";
-import {PlayerListComponent} from "./player-list/player-list.component";
-import {PlayerDetailComponent} from "./player-detail/player-detail.component";
+import {PlayerComponent} from "./components/player/player.component";
+import {PlayerListComponent} from "./components/player-list/player-list.component";
+import {PlayerDetailComponent} from "./components/player-detail/player-detail.component";
 import {HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from "@angular/material/card";
 import {MatTableModule} from "@angular/material/table";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatListModule} from "@angular/material/list";
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerComponent,
     PlayerListComponent,
-    PlayerDetailComponent
+    PlayerDetailComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
